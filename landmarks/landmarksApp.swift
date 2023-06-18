8afd32bf27e918ee107572b57021d9eafd4438d7
tian <istiancz@gmail.com>
2023-06-18 20:24:53 +0800

    Initial Commit

---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by tcz on 2023/6/18.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
