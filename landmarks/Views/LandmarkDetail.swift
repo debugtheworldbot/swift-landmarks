2b3f3e096f57f65b021b39ab0f9dc7aa5be19009
tian <istiancz@gmail.com>
2023-06-18 23:01:50 +0800

    display fetched data

--- landmarks/Views/LandmarkDetail.swift
+++ landmarks/Views/LandmarkDetail.swift
@@ -8,51 +8,53 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    var landmark: Landmark
     var body: some View {
-       
-        VStack{
+
             
-            MapView()
-                .ignoresSafeArea(edges:.top)
+        ScrollView{
+        VStack{
+         
+            MapView(coordinate: landmark.locationCoordinate)
                 .frame(height:300)
-                
-            CircleImage()
+
+            CircleImage(image: landmark.image)
                 .offset(y:-130)
                 .padding(.bottom,-130)
-            VStack(alignment: .leading) {
-                Text("turple")
-                    .font(.title)
-                
-                HStack {
-                    Text("Joshua Tree National Park")
-                        .font(.subheadline)
-                    Spacer()
+      
+                VStack(alignment: .leading) {
+                    Text(landmark.name)
+                        .font(.title)
                     
-                    Text("California")
-                        .font(.subheadline)
+                    HStack {
+                        Text(landmark.park)
+                            .font(.subheadline)
+                        Spacer()
+                        
+                        Text(landmark.state)
+                            .font(.subheadline)
+                    }
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                    
+                    
+                    Divider()
+                    
+                    
+                    Text("About \(landmark.name)")
+                        .font(.title2)
+                    Text(landmark.description)
+                    
                 }
-                .font(.subheadline)
-                .foregroundColor(.secondary)
+                .padding(20)
                 
-                
-                Divider()
-
-
-                Text("About Turtle Rock")
-                    .font(.title2)
-                Text("Descriptive text goes here.")
-                
-            }
-            .padding(20)
+            }}
             
-            Spacer()
         }
-    
-    }
 }
 
 struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkDetail()
+        LandmarkDetail(landmark: landmarks[0])
     }
 }
